e0f9144b8916d9e85ee920f21f46b2ad36939b8a
Marwan <marwanabdalrady13@gmail.com>
2026-03-03 22:40:50 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
       <div>
         You cant update or delete or add data duo to json-server library we
         should use real database or server to solve this issue🤓, or you can
-        basicly download the repo and do <code>npm i</code>
+        basically download the repo and do <code>npm i</code>
       </div>
       <KanbanBoard />
     </>
