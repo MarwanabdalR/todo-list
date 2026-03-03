046815ac60ed4baa04294b349fa619c6db923323
Marwan <marwanabdalrady13@gmail.com>
2026-03-03 22:39:57 +0200

    done

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,11 @@ import KanbanBoard from "@/components/MainPage/KanbanBoard";
 export default function Home() {
   return (
     <>
-    <div>You cant update or delete or add data duo to json-server we should use real database or server to solve this issue🤓</div>
+      <div>
+        You cant update or delete or add data duo to json-server library we
+        should use real database or server to solve this issue🤓, or you can
+        basicly download the repo and do <code>npm i</code>
+      </div>
       <KanbanBoard />
     </>
   );
